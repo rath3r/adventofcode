68613fef2425320193adc4aa211269ab2c193e55
Thomas Meehan <thom.meehan@gmail.com>
2022-12-02 23:08:37 +0000

    2b 2022

--- 2022/day2/index.ts
+++ 2022/day2/index.ts
@@ -1,25 +1,28 @@
 import * as fs from 'fs';
 
-const inputData = fs.readFileSync('./day2/input-short.txt','utf8');
+const inputData = fs.readFileSync('./day2/input-full.txt','utf8');
 
 let values = inputData.split("\n");
 
-// A X rock = 1
-// B Y paper = 2
-// C Z scissors = 3
+// A rock = 1
+// B paper = 2
+// C scissors = 3
 // 0 lost
 // 3 draw
 // 6 win
+// X lose
+// Y draw
+// Z win
 let results:any = {
-    'A Y': 8,
-    'A X': 4,
-    'A Z': 3,
+    'A Y': 4,
+    'A X': 3,
+    'A Z': 8,
     'B X': 1,
     'B Y': 5,
     'B Z': 9,
-    'C Z': 6,
-    'C Y': 2,
-    'C X': 7
+    'C Z': 7,
+    'C Y': 6,
+    'C X': 2
 };
 let score:any = [];
 values.forEach((el, i, arr) => {
@@ -28,6 +31,5 @@ values.forEach((el, i, arr) => {
     }
 });
 
-console.log(score);
 console.log(score.reduce((accumulator:any, currentValue:any) => accumulator + currentValue,0));
 
